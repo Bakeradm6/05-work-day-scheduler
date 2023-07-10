615a2bf0fbc545b1997f322072093a7a06c939fc
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-07-10 12:01:15 -0500

    added setCurrentDay function

--- script.js
+++ script.js
@@ -21,10 +21,17 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  function timeClassSet(){
+
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  function readLocalStorage(){
+
+  }
   // TODO: Add code to display the current date in the header of the page.
-});
+  function setCurrentDay(){
+    var today= dayjs().format('dddd MMM D, YYYY')
+    $('#currentDay').text("Today is "+today)
+  }
